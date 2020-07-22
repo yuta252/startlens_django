7c2a81280247d9d87b15090b78ba1b3d9c4dbfbf
Yuta Nakano <nakano.yuta252@gmail.com>
2020-07-22 23:00:01 +0900

    修正

--- scripts/exec_collectstatic.sh
+++ scripts/exec_collectstatic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ubuntu/startlens/venv/bin/activate
-aws s3 cp s3://startlens-media-storage/config/.env /home/ubuntu/startlens/startlens_django/.env
+aws s3 cp s3://startlens-media-storage/config/.env?versionId=null /home/ubuntu/startlens/startlens_django/.env
 python /home/ubuntu/startlens/startlens_django/manage.py collectstatic --noinput
